8c725eabfce626064d14a3dd76b070cc9722b981
Jansen Mitchell <jansenmitchellp@gmail.com>
2023-03-20 12:45:27 -0500

    Created function to display game board.

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -1,4 +1,14 @@
 import tic_tac_toe
 import pytest
 
-# Tests for the printed game board.
+def test_print_board():
+    tic_tac_toe.board = ["-", "-", "-",
+                         "-", "-", "-",
+                         "-", "-", "-"]
+    tic_tac_toe.current_player = "X"
+    tic_tac_toe.winner = None
+    tic_tac_toe.game_running = True
+    tic_tac_toe.print_board()
+    assert tic_tac_toe.board == ["-", "-", "-",
+                                 "-", "-", "-",
+                                 "-", "-", "-"]
