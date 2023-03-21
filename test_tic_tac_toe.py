41375580106124e237b16ab977dba788d23b4be5
Jansen Mitchell <jansenmitchellp@gmail.com>
2023-03-21 15:38:10 -0500

    improved test quality

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -1,23 +1,23 @@
 import tic_tac_toe
 import pytest
 
+# Tests that a board is printed correctly
 def test_print_board():
-    tic_tac_toe.board = ["-", "-", "-",
-                         "-", "-", "-",
-                         "-", "-", "-"]
-    tic_tac_toe.current_player = "X"
-    tic_tac_toe.winner = None
-    tic_tac_toe.game_running = True
-    tic_tac_toe.print_board()
-    assert tic_tac_toe.board == ["-", "-", "-",
-                                 "-", "-", "-",
-                                 "-", "-", "-"]
+    board = ["-", "-", "-",
+             "-", "-", "-",
+             "-", "-", "-"]
+    assert tic_tac_toe.print_board() == print(board[0] + " | " + board[1] + " | " + board[2])
+    assert tic_tac_toe.print_board() == print(board[3] + " | " + board[4] + " | " + board[5])
+    assert tic_tac_toe.print_board() == print(board[6] + " | " + board[7] + " | " + board[8])
     
-def test_player_input():
-    tic_tac_toe.board = ["-", "-", "-",
-                         "-", "-", "-",
-                         "-", "-", "-"]
-    tic_tac_toe.current_player = "X"
-    tic_tac_toe.winner = None
-    tic_tac_toe.game_running = True
-    tic_tac_toe.player_input()
+    
+# Tests that the player can input a valid position
+#def test_player_input():
+
+
+# Tests if current player has won horizontally    
+#def test_check_horizontal():
+
+    
+# Tests if current player has won vertically
+#def test_check_vertical():
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,21 +6,23 @@ winner = None
 game_running = True
 
 def print_board():
-    print(board[0] + "|" + board[1] + "|" + board[2])
-    print("-+-+-")
-    print(board[3] + "|" + board[4] + "|" + board[5])
-    print("-+-+-")
-    print(board[6] + "|" + board[7] + "|" + board[8])
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+#def player_input():
     
-def player_input():
-    position = int(input("Choose a position from 1-9: "))
     # Check if the position is valid
-    if position >= 1 and position <= 9 and board[position - 1] == "-":
+  
         # If it is, set the position to the current player
-        board[position - 1] = current_player
-    else:
-        print("Invalid position")
+
         
-while game_running:
-    print_board()
-    player_input()
+#def check_horizontal():
+    # Check if the current player has won horizontally
+
+    # Check if the first row has the same value
+    
+        
+#while game_running:
+print_board()
+    #player_input()
