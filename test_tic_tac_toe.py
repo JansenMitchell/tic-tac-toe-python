0bad1e24a9af63050429b07a086a1f6f42318ad2
Jansen Mitchell <jansenmitchellp@gmail.com>
2023-03-26 10:49:31 -0500

    created draw board function

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -1,68 +1,24 @@
 import tic_tac_toe
 import pytest
 
-# Test board exists.
-def test_board_init():
-    board = tic_tac_toe.Board()
-    assert board
-    
-# Test board is empty.
-def test_board_is_empty():
-    board = tic_tac_toe.Board()
-    assert board.board == [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-
-# Test board is full.
-def test_board_is_full():
-    board = tic_tac_toe.Board()
-    board.board = [["X", "X", "O"], ["O", "O", "X"], ["X", "O", "X"]]
-    assert board.is_full()
-
-# Test board resets.
-def test_board_reset():
-    board = tic_tac_toe.Board()
-    board.board = [["X", "X", "O"], ["O", "O", "X"], ["X", "O", "X"]]
-    board.reset()
-    assert board.board == [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-
-# Test board is printed as a 3x3 grid.
-def test_board_print():
-    board = tic_tac_toe.Board()
-    board.board = [["X", "X", "O"], ["O", "O", "X"], ["X", "O", "X"]]
-    # Three rows of 3 columns with dividers.
-    assert board.__str__() == "X | X | O \n---------\nO | O | X \n---------\nX | O | X \n"
-
-# Test board receives player 1 input.
-
-# Test board receives player 2 input.
-
-# Test player 1 exists.
-
-# Test player 2 exists.
-
-# Test game exists.
-
-# Test horizontal win.
-
-# Test vertical win.
-
-# Test diagonal win.
-
-# Test game is a draw.
-
-# Test game is running.
-
-# Test game is over.
-
-# Test is player 1 is X.
-
-# Test is player 2 is O.
-
-# Test player 1 is human player.
-
-# Test player 2 is ai player.
-
-# Test player 1 has made a move.
-
-# Test player 2 has made a move.
-
-# Test player 2 makes optimal move (minimax).
+def test_draw_board():
+    board = [' '] * 10
+    board[1] = 'X'
+    board[2] = 'O'
+    board[3] = 'X'
+    board[4] = 'O'
+    board[5] = 'X'
+    board[6] = 'O'
+    board[7] = 'X'
+    board[8] = 'O'
+    board[9] = 'X'
+    tic_tac_toe.draw_board(board)
+    assert board[1] == 'X'
+    assert board[2] == 'O'
+    assert board[3] == 'X'
+    assert board[4] == 'O'
+    assert board[5] == 'X'
+    assert board[6] == 'O'
+    assert board[7] == 'X'
+    assert board[8] == 'O'
+    assert board[9] == 'X'
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,16 +1,10 @@
-# Creates the board.
-class Board:
-    def __init__(self):
-        self.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-        
-    def is_empty(self):
-        return self.board == [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-    
-    def is_full(self):
-        return not any(" " in row for row in self.board)
-    
-    def reset(self):
-        self.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-        
-    def __str__(self):
-        return f"{self.board[0][0]} | {self.board[0][1]} | {self.board[0][2]} \n---------\n{self.board[1][0]} | {self.board[1][1]} | {self.board[1][2]} \n---------\n{self.board[2][0]} | {self.board[2][1]} | {self.board[2][2]} \n"
+import random
+
+def draw_board(board):
+    # This function prints out the board that it was passed.
+    # "board" is a list of 10 strings representing the board (ignore index 0)
+    print(board[1] + '|' + board[2] + '|' + board[3])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[7] + '|' + board[8] + '|' + board[9])
