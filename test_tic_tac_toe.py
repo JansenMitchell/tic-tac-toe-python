11a98da24484c401913754b2f83a9e8300883cf5
Jansen Mitchell <jansenmitchellp@gmail.com>
2023-03-27 16:23:28 -0500

    new board

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -2,56 +2,5 @@ import tic_tac_toe
 import pytest
 
 def test_draw_board():
-    board = [' '] * 10
-    board[1] = 'X'
-    board[2] = 'O'
-    board[3] = 'X'
-    board[4] = 'O'
-    board[5] = 'X'
-    board[6] = 'O'
-    board[7] = 'X'
-    board[8] = 'O'
-    board[9] = 'X'
-    tic_tac_toe.draw_board(board)
-    assert board[1] == 'X'
-    assert board[2] == 'O'
-    assert board[3] == 'X'
-    assert board[4] == 'O'
-    assert board[5] == 'X'
-    assert board[6] == 'O'
-    assert board[7] == 'X'
-    assert board[8] == 'O'
-    assert board[9] == 'X'
-    
-def test_is_space_free():
-    board = [' '] * 10
-    board[1] = 'X'
-    board[2] = 'O'
-    board[3] = 'X'
-    board[4] = 'O'
-    board[5] = 'X'
-    board[6] = 'O'
-    board[7] = 'X'
-    board[8] = 'O'
-    board[9] = 'X'
-    assert tic_tac_toe.is_space_free(board, 1) == False
-    assert tic_tac_toe.is_space_free(board, 2) == False
-    assert tic_tac_toe.is_space_free(board, 3) == False
-    assert tic_tac_toe.is_space_free(board, 4) == False
-    assert tic_tac_toe.is_space_free(board, 5) == False
-    assert tic_tac_toe.is_space_free(board, 6) == False
-    assert tic_tac_toe.is_space_free(board, 7) == False
-    assert tic_tac_toe.is_space_free(board, 8) == False
-    assert tic_tac_toe.is_space_free(board, 9) == False
-    
-def test_get_player_move():
-    board = [' '] * 10
-    assert tic_tac_toe.get_player_move(board) == 1
-    assert tic_tac_toe.get_player_move(board) == 2
-    assert tic_tac_toe.get_player_move(board) == 3
-    assert tic_tac_toe.get_player_move(board) == 4
-    assert tic_tac_toe.get_player_move(board) == 5
-    assert tic_tac_toe.get_player_move(board) == 6
-    assert tic_tac_toe.get_player_move(board) == 7
-    assert tic_tac_toe.get_player_move(board) == 8
-    assert tic_tac_toe.get_player_move(board) == 9
+    board = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X']
+    expected = ('-' * 13 + '| X | O | X |' + '-' * 13 + '| O | X | O |' + '-' * 13 + '| X | O | X |' + '-' * 13)
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,23 +1,10 @@
 import random
 
 def draw_board(board):
-    # This function prints out the board that it was passed.
-    # "board" is a list of 10 strings representing the board (ignore index 0)
-    print(board[1] + '|' + board[2] + '|' + board[3])
-    print('-+-+-')
-    print(board[4] + '|' + board[5] + '|' + board[6])
-    print('-+-+-')
-    print(board[7] + '|' + board[8] + '|' + board[9])
-    
-def is_space_free(board, move):
-    # Return true if the passed move is free on the passed board.
-    return board[move] == ' '
-
-def get_player_move(board):
-    while True:
-        print('What is your next move? (1-9)')
-        move = input()
-        if move in '1 2 3 4 5 6 7 8 9'.split() and is_space_free(board, int(move)):
-            return int(move)
-        else:
-            print('That is not a valid move. Choose again.')
+    print('-' * 13)
+    print('| ' + board[0] + ' | ' + board[1] + ' | ' + board[2] + ' |')
+    print('-' * 13)
+    print('| ' + board[3] + ' | ' + board[4] + ' | ' + board[5] + ' |')
+    print('-' * 13)
+    print('| ' + board[6] + ' | ' + board[7] + ' | ' + board[8] + ' |')
+    print('-' * 13)
