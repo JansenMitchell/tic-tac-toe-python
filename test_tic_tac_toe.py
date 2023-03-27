17b91a546f4ecc4fe05d02ac18675edc1b10a449
Jansen Mitchell <jansenmitchellp@gmail.com>
2023-03-27 15:22:31 -0500

    created is space free

--- test_tic_tac_toe.py
+++ test_tic_tac_toe.py
@@ -22,3 +22,24 @@ def test_draw_board():
     assert board[7] == 'X'
     assert board[8] == 'O'
     assert board[9] == 'X'
+    
+def test_is_space_free():
+    board = [' '] * 10
+    board[1] = 'X'
+    board[2] = 'O'
+    board[3] = 'X'
+    board[4] = 'O'
+    board[5] = 'X'
+    board[6] = 'O'
+    board[7] = 'X'
+    board[8] = 'O'
+    board[9] = 'X'
+    assert tic_tac_toe.is_space_free(board, 1) == False
+    assert tic_tac_toe.is_space_free(board, 2) == False
+    assert tic_tac_toe.is_space_free(board, 3) == False
+    assert tic_tac_toe.is_space_free(board, 4) == False
+    assert tic_tac_toe.is_space_free(board, 5) == False
+    assert tic_tac_toe.is_space_free(board, 6) == False
+    assert tic_tac_toe.is_space_free(board, 7) == False
+    assert tic_tac_toe.is_space_free(board, 8) == False
+    assert tic_tac_toe.is_space_free(board, 9) == False
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,3 +8,16 @@ def draw_board(board):
     print(board[4] + '|' + board[5] + '|' + board[6])
     print('-+-+-')
     print(board[7] + '|' + board[8] + '|' + board[9])
+    
+def is_space_free(board, move):
+    # Return true if the passed move is free on the passed board.
+    return board[move] == ' '
+
+def get_player_move(board):
+    while True:
+        print('What is your next move? (1-9)')
+        move = input()
+        if move in '1 2 3 4 5 6 7 8 9'.split() and is_space_free(board, int(move)):
+            return int(move)
+        else:
+            print('That is not a valid move. Choose again.')
